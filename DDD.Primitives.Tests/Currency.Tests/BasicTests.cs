283c79970efe5b9ec7ca6c082817798eceb78293
Tadeusz Gruzlewski <tadeusz.gruzlewski@consart.nl>
2023-03-22 17:17:19 +0100

    simple tests added

--- DDD.Primitives.Tests/Currency.Tests/BasicTests.cs
+++ DDD.Primitives.Tests/Currency.Tests/BasicTests.cs
@@ -1,5 +1,6 @@
 using DDD.Primitives.Finance;
 using NUnit.Framework;
+using System;
 
 namespace CurrencyTests;
 
@@ -14,6 +15,20 @@ public class BasicTests
         Assert.AreEqual(Currency.USD.Symbol, "$");
         Assert.AreEqual(Currency.USD.Entity, "United States dollar");
         Assert.AreEqual(Currency.USD.Name, "United States dollar");
+    }
+
+    [Test]
+    public void PriceTestOK()
+    {
+        var p = new Price(Currency.USD, 10);
+
+        Assert.AreEqual(p.Amount, 10);
+    }
+
+    [Test]
+    public void PriceTestFailure()
+    {
+        Assert.Throws<ArgumentException>(() => new Price(Currency.USD, 0));
     }
 
 }
